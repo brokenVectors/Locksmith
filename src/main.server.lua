c257894f72605afdce94e076d6b914ee68b38105
brokenVectors <brokenvectors@gmail.com>
2020-07-20 16:31:11 +0200

    UI Buttons done

--- src/main.server.lua
+++ src/main.server.lua
@@ -20,9 +20,26 @@ local root = script.Parent.Parent
 local pluginFrame = root.PluginFrame
 
 local pages = require(script.Parent.pages)
+local pageList = {Buttons = pluginFrame.Buttons, ScanGame = pluginFrame.ScanGame, ScanPlugins = pluginFrame.ScanPlugins}
+local buttons = {ScanGame = pageList.Buttons.ScanGameBtn, ScanPlugins = pageList.Buttons.ScanPluginsBtn}
 
-pages.Init({Buttons = pluginFrame.Buttons, Scan = pluginFrame.Scan}, "Buttons")
-
-print( pages.GetPageByName("Buttons") )
+pages.Init(pageList, "Buttons")
 pluginFrame.Parent = widget
-wait(1)
+
+
+
+buttons.ScanGame.MouseButton1Down:Connect(function()
+    pages.Open("ScanGame")
+end)
+
+buttons.ScanPlugins.MouseButton1Down:Connect(function()
+    pages.Open("ScanPlugins")
+end)
+
+pageList.ScanGame.BackBtn.MouseButton1Down:Connect(function()
+    pages.Open("Buttons")
+end)
+
+pageList.ScanPlugins.BackBtn.MouseButton1Down:Connect(function()
+    pages.Open("Buttons")
+end)
